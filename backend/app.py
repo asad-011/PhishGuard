65beaa0cce6bf85578ea42d22ee9a6bbb3908979
asad-011 <asadmohd543@gmail.com>
2026-05-07 05:02:14 +0530

    route

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,10 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "PhishGuard Backend Running"
+
 @app.route("/analyze", methods=["POST"])
 def analyze_email():
     try:
